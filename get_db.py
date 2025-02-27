1dd99e2d4fbe03d4c57c637a87b25cbd972b168a
Armada3334 <contact@sapherians.com>
2025-02-27 15:11:06 -0500

    make db module

--- get_db.py
+++ get_db.py
@@ -5,14 +5,15 @@ import os
 
 load_dotenv()
 
-# Server configuration
-url = os.environ["URL"]
+def get_db():
+    # Server configuration
+    url = os.environ["URL"]
+    context = ssl._create_unverified_context()
+    common = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/db", context=context)
+    # Get the list of databases
+    db_list = common.list()
+    return db_list
 
-context = ssl._create_unverified_context()
-common = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/db", context=context)
 
-
-# Get the list of databases
-db_list = common.list()
-
-print("Available Databases:", db_list)
+if __name__ == "__main__":
+    print(get_db())
